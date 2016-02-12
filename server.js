4b5b89b256252405e674a6516c7d29e1bae97455
steph-harris <steph.harris21@gmail.com>
2016-02-12 12:39:37 -0500

    linked css and js files, materialize cdns, and jQuery

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var PORT = process.env.NODE_ENV || 8080;
 app.use("/css", express.static("public/css"));
 app.use("/js", express.static("public/js"));
 
+app.get("/", function(req, res){
+  res.sendFile(process.cwd() + "/views/home.html");
+});
+
 app.listen(PORT, function(){
   console.log("Listening on port %s", PORT);
 });
