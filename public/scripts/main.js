6ac03e1c20fac5877f3f15c4aa89e39f46256b87
steph-harris <steph.harris21@gmail.com>
2016-02-16 13:10:53 -0500

    changed about section color

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+
+  // var options = [
+  //   {selector: '.card-panel', offset: -50, callback: 'Materialize.fadeInImage(".card-panel")' }
+  // ];
+
+  // Materialize.scrollFire(options);
 });
